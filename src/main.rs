6adc5a2e656ae3c1ee2f8ab0cbb68a1f0252f059
unyime1 <lordunyime@gmail.com>
2022-02-21 17:00:31 +0100

    completed wireup

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,31 @@ mod processes;
 mod state;
 mod todo;
 
+use processes::process_input;
 use serde_json::value::Value;
 use serde_json::{json, Map};
-use state::{read_file, write_to_file};
+use state::read_file;
 use std::env;
+use todo::todo_factory;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let status: &String = &args[1];
+    let command: &String = &args[1];
     let title: &String = &args[2];
 
-    let mut state: Map<String, Value> = read_file("./state.json");
-    println!("{:?}", state);
-    state.insert(title.to_string(), json!(status));
-    write_to_file("./state.json", &mut state);
+    let state: Map<String, Value> = read_file("./state.json");
+
+    let status: String;
+
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        }
+        None => {
+            status = String::from("pending");
+        }
+    }
+
+    let item = todo_factory(&status, title).expect(&status);
+    process_input(item, command.to_string(), &state);
 }
