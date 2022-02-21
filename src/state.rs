830780e59795907f0a8e1dd9dc634abb8e8301d3
unyime1 <lordunyime@gmail.com>
2022-02-21 16:33:47 +0100

    added crud capabilities to traits

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,4 @@
-use std::fs::{write, File, read_to_string};
+use std::fs::{read_to_string, write, File};
 use std::io::Read;
 
 use serde_json::json;
@@ -10,7 +10,7 @@ pub fn read_file(file_name: &str) -> Map<String, Value> {
     let data = read_to_string(&file_name).unwrap();
     let json: Value = serde_json::from_str(&data).unwrap();
     let state: Map<String, Value> = json.as_object().unwrap().clone();
-    return state
+    return state;
 }
 
 // write data to json
--- src/todo/structs/traits/delete.rs
+++ src/todo/structs/traits/delete.rs
@@ -1,5 +1,12 @@
+use serde_json::Map;
+use serde_json::value::Value;
+
+use crate::state::write_to_file
+
 pub trait Delete {
-    fn delete(&self, title: &str) {
-        println!("{} is being deleted", title);
+    fn delete(&self, title: &str, state: &Map<String, Value>) {
+        state.remove(title);
+        write_to_file("./state.json", &mut state);
+        println!("\n\n{} is being deleted\n\n", title);
     }
 }
