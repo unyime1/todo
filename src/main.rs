dc466325be4f60b1430b4f6fcc22d6133985ba4e
unyime1 <lordunyime@gmail.com>
2022-02-21 16:04:39 +0100

    added read and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-mod todo;
+mod state;
 
-use todo::{todo_factory, ItemTypes};
-use todo::structs::traits::create::Create;
+use serde_json::value::Value;
+use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
 
 fn main() {
-    let todo_item: Result<ItemTypes, &'static str> = todo_factory("pending", "make");
-    match todo_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
 
-        ItemTypes::Done(item) => println!(
-            "it's a done item with the title: {}",
-            item.super_struct.title
-        ),
-    }
+    let mut state: Map<String, Value> = read_file("./state.json");
+    println!("{:?}", state);
+    state.insert(title.to_string(), json!(status));
+    write_to_file("./state.json", &mut state);
 }
---
+++ src/state.rs
@@ -0,0 +1,20 @@
+use std::fs::{write, File, read_to_string};
+use std::io::Read;
+
+use serde_json::json;
+use serde_json::value::Value;
+use serde_json::Map;
+
+/// Read file to json
+pub fn read_file(file_name: &str) -> Map<String, Value> {
+    let data = read_to_string(&file_name).unwrap();
+    let json: Value = serde_json::from_str(&data).unwrap();
+    let state: Map<String, Value> = json.as_object().unwrap().clone();
+    return state
+}
+
+// write data to json
+pub fn write_to_file(file_name: &str, state: &mut Map<String, Value>) {
+    let new_data = json!(state);
+    write(file_name.to_string(), new_data.to_string()).expect("Unable to write file.");
+}
